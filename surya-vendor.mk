77854134e4490228946dfbe61c5a86d04769549a
Willy Gardiol <willy@gardiol.org>
2022-09-01 08:06:22 +0200

    surya:  surya: Drop thermal-engine
    * mi_thermald does it's job.
    
    Change-Id: Ifec5716b1056a9b23a1946e63537c49d9de81623

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -195,7 +195,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-arvr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-arvr.conf \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-camera.conf \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-chg-only.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-chg-only.conf \
-    vendor/xiaomi/surya/proprietary/vendor/etc/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-map.conf \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-nolimits.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-nolimits.conf \
     vendor/xiaomi/surya/proprietary/vendor/etc/thermal-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-normal.conf \
@@ -859,7 +858,6 @@ PRODUCT_PACKAGES += \
     sensors.qti \
     slim_daemon \
     tftp_server \
-    thermal-engine \
     time_daemon \
     vibrator_calibration \
     vppservice \
