17d8c4dfb3569d05be1ce80efbad3a6979751537
Willy Gardiol <willy@gardiol.org>
2023-04-10 20:56:27 +0200

    surya: Remove no longer needed aptX blobs
    Change-Id: I405800f9dc85f381ae0c9cea36cfe72a24ad0f82

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -811,8 +811,6 @@ PRODUCT_PACKAGES += \
     lib-imsvt \
     lib-imsvtextutils \
     lib-imsvtutils \
-    libaptXHD_encoder \
-    libaptX_encoder \
     libdiag_system \
     libdpmctmgr \
     libdpmfdmgr \
